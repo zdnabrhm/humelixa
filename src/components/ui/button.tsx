73dfeab41e5bce8467606e1b2047c52a3229a0ea
zdnabrhm <zidangit07@gmail.com>
2025-12-27 06:34:42 +0800

    style: add font-medium to button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap font-mono uppercase tracking-widest cursor-pointer select-none outline-none transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 shrink-0 group/button",
+  "inline-flex items-center justify-center whitespace-nowrap font-medium font-mono uppercase tracking-widest cursor-pointer select-none outline-none transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 shrink-0 group/button",
   {
     variants: {
       variant: {
