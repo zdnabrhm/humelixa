919ad22ab6ea497a7456227dc4b12dabb70699c9
zdnabrhm <zidangit07@gmail.com>
2026-01-01 11:09:57 +0800

    fix: handle Google private key newlines correctly

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,20 @@
 /// <reference path="../.astro/types.d.ts" />
 
-interface Env {
+type Runtime = import("@astrojs/cloudflare").Runtime<{
   TURNSTILE_SECRET_KEY: string;
   GOOGLE_PRIVATE_KEY: string;
   GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
   SPREADSHEET_ID: string;
-}
-
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+}>;
 
 declare namespace App {
   interface Locals extends Runtime {}
+}
+
+interface ImportMetaEnv {
+  readonly PUBLIC_TURNSTILE_SITE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
